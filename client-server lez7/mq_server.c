f080da9ead5d021ab79d41e6201dc0065722ae63
domenico <vichidomenico98@hotmail.com>
2022-10-25 22:54:57 +0200

    inserimento commenti

--- client-server lez7/mq_server.c
+++ client-server lez7/mq_server.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]){
     long serial_number = 1; /*numero seriale da inviare al client*/
     char client_queue_name[16]; //nome coda del client
     struct mq_attr attr; //attributi della coda
-    attr.mq_flags = 0; //bloccante 
+    attr.mq_flags = 0; //bloccante quindi sincrono
     attr.mq_maxmsg = MAX_MESSAGES; /*numero massimo di messaggi nella coda*/
     attr.mq_msgsize = sizeof(msg_rcv);
     attr.mq_curmsgs = 0;
@@ -32,7 +32,8 @@ int main(int argc, char *argv[]){
         /*preleva dalla coda il messaggio con priorità più alta*/
         mq_receive (qd_server, (char*)& msg_rcv,sizeof(msg_rcv),NULL);
         printf("Server: messaggio ricevuto dal client %d, %s. \n",msg_rcv.pid,msg_rcv.text);
-        sprintf(client_queue_name,"/mq_%d", msg_rcv.pid);
+        sprintf(client_queue_name,"/mq_%d", msg_rcv.pid);  //salva dentro client_queue name, msg_rcv.pid
+        
         /*invia il messaggio di risposta al client*/
         qd_client = mq_open(client_queue_name, O_WRONLY);
         sprintf(msg_send.text, "benvenuto client %d, il tuo numero è %ld", msg_rcv.pid, serial_number);
